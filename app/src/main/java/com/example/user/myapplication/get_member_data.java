f9696437d26a81b26406f961e6ce3b9fb54aa4af
zheng <esz759486@gmail.com>
2017-12-09 22:15:14 +0800

    未處理資料大量跑版

--- app/src/main/java/com/example/user/myapplication/get_member_data.java
+++ app/src/main/java/com/example/user/myapplication/get_member_data.java
@@ -2,11 +2,14 @@ package com.example.user.myapplication;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -72,10 +75,79 @@ public class get_member_data extends AsyncTask<String, Void, String> {
 
     @Override
     protected void onPostExecute(String result){
-        Toast.makeText(context,result,Toast.LENGTH_SHORT).show();
         try {
             JSONObject jsonObject = new JSONObject(result);
-            JSONObject member = new JSONObject(jsonObject.getString("member"));
+            JSONArray now_month_data = new JSONArray(jsonObject.getString("now_month_data"));
+
+
+
+//            Toast.makeText(context,jsonObject.getString("now_month_data").length(),Toast.LENGTH_SHORT).show();
+            if(now_month_data.length()>0) {
+                member_data_layout.removeAllViews();
+                for (int i = 0; i < now_month_data.length(); i++) {
+                    String status_on = now_month_data.getJSONObject(i).getString("status_on");
+                    String status_off = now_month_data.getJSONObject(i).getString("status_off");
+                    String work_on_time = now_month_data.getJSONObject(i).getString("work_on_time");
+                    String work_off_time = now_month_data.getJSONObject(i).getString("work_off_time");
+                    String work_time = now_month_data.getJSONObject(i).getString("work_time");
+                    member_data_layout.setLayoutParams(new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.WRAP_CONTENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT, 1
+                    ));
+                    LinearLayout data_layout = new LinearLayout(context);
+                    data_layout.setLayoutParams(new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT, 1
+                    ));
+//                /*上班打卡狀態*/
+//                TextView status_on_textview = new TextView(context);
+//                status_on_textview.setText(status_on);
+//                status_on_textview.setGravity(Gravity.CENTER);
+//                status_on_textview.setLayoutParams(new LinearLayout.LayoutParams(
+//                        LinearLayout.LayoutParams.WRAP_CONTENT,
+//                        LinearLayout.LayoutParams.WRAP_CONTENT
+//                ));
+//                /*下班打卡狀態*/
+//                TextView status_off_textview = new TextView(context);
+//                status_off_textview.setText(status_off);
+//                status_off_textview.setGravity(Gravity.CENTER);
+//                status_off_textview.setLayoutParams(new LinearLayout.LayoutParams(
+//                        LinearLayout.LayoutParams.WRAP_CONTENT,
+//                        LinearLayout.LayoutParams.WRAP_CONTENT
+//                ));
+                /*上班打卡時間*/
+                    TextView work_on_time_textview = new TextView(context);
+                    work_on_time_textview.setText(work_on_time);
+                    work_on_time_textview.setGravity(Gravity.CENTER);
+                    work_on_time_textview.setLayoutParams(new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.WRAP_CONTENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT, 1
+                    ));
+                /*下班打卡時間*/
+                    TextView work_off_time_textview = new TextView(context);
+                    work_off_time_textview.setText(work_off_time);
+                    work_off_time_textview.setGravity(Gravity.CENTER);
+                    work_off_time_textview.setLayoutParams(new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.WRAP_CONTENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT, 1
+                    ));
+                /*當日工作時數*/
+                    TextView work_time_textview = new TextView(context);
+                    work_time_textview.setText(work_time);
+                    work_time_textview.setGravity(Gravity.CENTER);
+                    work_time_textview.setLayoutParams(new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.WRAP_CONTENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT, 1
+                    ));
+                    data_layout.addView(work_on_time_textview);
+                    data_layout.addView(work_off_time_textview);
+                    data_layout.addView(work_time_textview);
+                    member_data_layout.addView(data_layout);
+                }
+            }
+            else{
+                member_data_layout.removeAllViews();
+            }
         } catch (JSONException e) {
             e.printStackTrace();
         }
